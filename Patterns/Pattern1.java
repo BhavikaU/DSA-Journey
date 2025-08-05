0d67e4a6be007d771147affa6d018a7bce6771b1
BhavikaU <uppalabhavika04@gmail.com>
2025-08-05 21:38:01 +0530

    added comments and converted to Rectangular Pattern

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -7,11 +7,15 @@ public class Pattern1 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // Number of rows 
        // int n ; // Number of rows
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= n; i++) // This is the outer loop which will loop for the rows.
+        {
+            for (int j = 1; j <= n; j++)  /// This is the inner loop which here, loops for the columns
+            {
                 System.out.print("* ");
             }
-            System.out.println();
+            System.out.println();  /// As soon as N stars are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
         }
     }
     
