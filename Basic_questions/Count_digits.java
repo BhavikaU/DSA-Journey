7e03bfea9a95bdbd83ad1fb3682187ec5acfaf7b
BhavikaU <uppalabhavika04@gmail.com>
2025-08-05 23:32:51 +0530

    Brute Fore Approch Of Counting of Digits

--- Basic_questions/Count_digits.java
+++ Basic_questions/Count_digits.java
@@ -1,1 +1,15 @@
-// Brute Fore Approch
+// 1st Brute Fore Approch
+package Basic_questions;
+import java.util.Scanner;
+public class Count_digits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Number of rows
+        int count = 0; // Initialize count of digits
+        while (n > 0) {
+            n /= 10; // Remove the last digit
+            count++; // Increment count
+        }
+        System.out.println(count); // Print the total count of digits
+    }
+}
