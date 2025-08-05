9d50d175426ec710abef15e0afaaa41f10d790c0
BhavikaU <uppalabhavika04@gmail.com>
2025-08-05 21:46:45 +0530

    Right Angled Triangle Pattern Solved

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Pattern2 {
+    public static void main(String[] args)
+    {
+        int n = 4;
+        for (int i = 1; i <= n; i++) // Outer loop for rows
+        {
+            for (int j = 1; j <= i; j++) // Inner loop for columns
+            {
+                System.out.print("* "); // Print star followed by space
+            }
+            System.out.println(); // Move to the next line after printing stars in a row
+        }
+    }
+}
